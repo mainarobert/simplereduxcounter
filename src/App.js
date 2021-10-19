28f6bb29282dfd06d1ecafc829f996005e3b2345
RobertMaina <robertmaina254@gmail.com>
2021-10-19 12:32:52 +0300

    simple redux counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./redux/counter";
 
 function App() {
+  const count = useSelector( state => state.counter.count);
+  const dispach = useDispatch()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The count is: {count}</h1>
+      <button onClick={()=>dispach(increment())}>increment</button>
+      <button onClick={()=>dispach(decrement())}>decrement</button>
+      <button onClick={()=>dispach(incrementByAmount(5))}>increment by 5</button>
     </div>
   );
 }
